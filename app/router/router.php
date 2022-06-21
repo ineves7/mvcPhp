2ac730c9e4f135dc1c04acda55af9e88ffb9c2b7
IAGO NEVES <86116557+ineves7@users.noreply.github.com>
2022-06-21 15:34:16 -0300

    First commit
    Estrutura mvc criada, e sendo estruturada a cada commit

---
+++ app/router/router.php
@@ -0,0 +1,24 @@
+<?php
+
+function routes()
+{
+    return require 'routes.php';
+}
+
+function exactMatchUriInArrayRoutes($uri, $routes){
+    if(array_key_exists($uri, $routes)){
+        return [];
+    }
+
+    return[];
+
+}
+
+function router()
+{
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    
+    $routes = routes();
+
+    
+}
---
+++ app/router/routes.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+        '/'=> 'Home@index',
+        '/user/create' => 'User@create'
+    ];
